a5d656e026c094d6f5294a9861baf9c3029f67d3
sabrinacathalo <sabrina.cathalo@epsi.fr>
2022-01-04 11:17:40 +0100

    Modifiy code

--- src/java/fr/epsi/Application.java
+++ src/java/fr/epsi/Application.java
@@ -5,7 +5,6 @@ import static fr.epsi.http.httpGet.httpGet;
 public class Application {
 
     public static void main(String[] args) {
-        String url_staff = "https://raw.githubusercontent.com/Bryan82370/MSPR_1_DATA/main/staff.txt";
-        httpGet(url_staff);
+        httpGet();
     }
 }
